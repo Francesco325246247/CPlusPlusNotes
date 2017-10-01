19bbcc4798cebca44106ada4ae2dc4e91775a3c1
Francesco <Francesco325246247@users.noreply.github.com>
2017-10-01 13:26:35 +0200

    Update cout.precision.cpp

--- cout.precision.cpp
+++ cout.precision.cpp
@@ -7,7 +7,7 @@ int main(void)
 long double PI;
 double PI2;
 
-PI = 3.14159265358979323846264338327950288419716939937510;
+PI = 3.14159265358979323846264338327950288419716939937510L;
 PI2 = 3.14159265358979323846264338327950288419716939937510;
 
 cout.precision(50); 
@@ -18,3 +18,9 @@ cout << "PI = 3.14159265358979323846264338327950288419716939937510" << endl;
 cout << "sizeof(double) = " << sizeof(double) << endl;
 cout << "sizeof(long double) = " << sizeof(long double) << endl;
 }
+
+
+//Don't define pi that way, because the number of digits has no effect on how precise a long double is. Do this instead:
+//long double pi=acos(-1.0L)
+//This will give pi to howver many digits the processor supports.
+//SEE piacos.cpp
