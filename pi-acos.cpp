8190eed385cf0ede699cab50d778294c31dce62d
Francesco <Francesco325246247@users.noreply.github.com>
2017-10-01 13:29:25 +0200

    PI=acos(-1.0L)

--- pi-acos.cpp
+++ pi-acos.cpp
@@ -13,3 +13,8 @@ int main(void)
 	cout << "PI = " << PI << endl;
 	cout << "PI = " << PI2 << endl;
 }
+
+//https://gmplib.org/#WHAT 
+//GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers. 
+//There is no practical limit to the precision except the ones implied by the available memory 
+//in the machine GMP runs on. GMP has a rich set of functions, and the functions have a regular interface.
